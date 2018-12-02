118b79ce8dadecc6eecf2ebcac1ac16221fd1899
h-j-k <han.junkwang@gmail.com>
2018-12-02 15:16:26 +0800

    Day 2 part 2

--- src/test/kotlin/com/ikueb/advent18/Day02Test.kt
+++ src/test/kotlin/com/ikueb/advent18/Day02Test.kt
@@ -4,7 +4,28 @@ import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 
 internal class Day02Test {
-    private val example = listOf(
+
+    @Test
+    fun partOneExample() {
+        Assertions.assertThat(getChecksum(partOneExample)).isEqualTo(12)
+    }
+
+    @Test
+    fun partOne() {
+        Assertions.assertThat(getChecksum(getInput("Day02"))).isEqualTo(4693)
+    }
+
+    @Test
+    fun partTwoExample() {
+        Assertions.assertThat(getCommonLetters(partTwoExample)).isEqualTo("fgij")
+    }
+
+    @Test
+    fun partTwo() {
+        Assertions.assertThat(getCommonLetters(getInput("Day02"))).isEqualTo("pebjqsalrdnckzfihvtxysomg")
+    }
+
+    private val partOneExample = listOf(
             "abcdef",
             "bababc",
             "abbcde",
@@ -14,13 +35,13 @@ internal class Day02Test {
             "ababab"
     )
 
-    @Test
-    fun example() {
-        Assertions.assertThat(getChecksum(example)).isEqualTo(12)
-    }
-
-    @Test
-    fun partOne() {
-        Assertions.assertThat(getChecksum(getInput("Day02"))).isEqualTo(4693)
-    }
+    private val partTwoExample = listOf(
+            "abcde",
+            "fghij",
+            "klmno",
+            "pqrst",
+            "fguij",
+            "axcye",
+            "wvxyz"
+    )
 }
