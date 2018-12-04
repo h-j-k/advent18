1271613e294b13e7784479d8c24d400a0676dee9
h-j-k <han.junkwang@gmail.com>
2018-12-05 01:09:15 +0800

    Refactored Day 4

--- src/main/kotlin/com/ikueb/advent18/Day04.kt
+++ src/main/kotlin/com/ikueb/advent18/Day04.kt
@@ -1,56 +1,53 @@
 package com.ikueb.advent18
 
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 object Day04 {
 
-    private const val PATTERN = "\\[1518-(..)-(..) (..):(..)\\] (.*)"
+    private val PARSER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
 
     fun strategyOne(input: List<String>): Int {
         return mapGuardsToNaps(input)
-                .maxBy { g -> g.value.sumBy { it.last - it.first + 1 } }!!
-                .let { it.key * mostCommon(it.value).first }
+                .maxBy { it.value.sumBy { nap -> nap.last - nap.first + 1 } }!!
+                .let { it.key * mostCommon(it.value).key }
     }
 
     fun strategyTwo(input: List<String>): Int {
         return mapGuardsToNaps(input)
                 .mapValues { mostCommon(it.value) }
-                .maxBy { it.value.second }!!
-                .let { it.key * it.value.first }
+                .maxBy { it.value.value }!!
+                .let { it.key * it.value.key }
     }
 
     private fun mapGuardsToNaps(input: List<String>): Map<Int, Set<IntRange>> {
-        val logs = input.parseWith(PATTERN) { (month, day, hour, minute, entry) ->
-            Log(LocalDateTime.of(2018, month.toInt(), day.toInt(), hour.toInt(), minute.toInt()), entry)
-        }.sorted()
         val results = mutableMapOf<Int, Set<IntRange>>()
-        var currentGuard = -1
+        var guard = -1
         var asleepAt = -1
-        for (log in logs) {
-            if (log.guardEntry != null) {
-                currentGuard = log.guardEntry
-                continue
-            }
-            if (log.isAsleepEntry) {
-                asleepAt = log.timestamp.minute
-                continue
-            }
-            if (log.isAwakeEntry) {
-                results.merge(currentGuard, setOf(asleepAt..(log.timestamp.minute - 1))) { a, b -> a.plus(b) }
+        input.parseWith("\\[(.*)\\] (.*)") { (timestamp, entry) ->
+            Log(LocalDateTime.parse(timestamp, PARSER), entry)
+        }.sorted().forEach { log ->
+            when {
+                log.guardEntry != null -> guard = log.guardEntry
+                log.isAsleepEntry -> asleepAt = log.minute
+                log.isAwakeEntry -> results.merge(
+                        guard, setOf(asleepAt..(log.minute - 1))) { a, b -> a.plus(b) }
             }
         }
         return results
     }
+}
 
-    private fun mostCommon(set: Set<IntRange>): Pair<Int, Int> {
-        return set.flatten().groupingBy { it }.eachCount().maxBy { it.value }!!.toPair()
-    }
+private fun mostCommon(set: Set<IntRange>): Map.Entry<Int, Int> {
+    return set.flatten().groupingBy { it }.eachCount().maxBy { it.value }!!
 }
 
 data class Log(val timestamp: LocalDateTime, val entry: String) : Comparable<Log> {
-    val guardEntry = "Guard #(\\d+) begins shift".toRegex().matchEntire(entry)?.let { it.groupValues[1].toInt() }
+    val guardEntry = "Guard #(\\d+) begins shift".toRegex()
+            .matchEntire(entry)?.let { it.groupValues[1].toInt() }
     val isAwakeEntry = entry == "wakes up"
     val isAsleepEntry = entry == "falls asleep"
+    val minute = timestamp.minute
 
     override fun compareTo(other: Log) = timestamp.compareTo(other.timestamp)
 }
