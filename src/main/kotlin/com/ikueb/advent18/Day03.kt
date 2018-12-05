09426deb67f5519631202d3915c068c5be00310a
h-j-k <han.junkwang@gmail.com>
2018-12-05 23:02:36 +0800

    Refactored Day 3 and 4

--- src/main/kotlin/com/ikueb/advent18/Day03.kt
+++ src/main/kotlin/com/ikueb/advent18/Day03.kt
@@ -1,7 +1,5 @@
 package com.ikueb.advent18
 
-private typealias Result = Pair<List<Claim>, Map<Point, Set<Claim>>>
-
 object Day03 {
 
     private const val PATTERN = "#(\\d+) @ (\\d+),(\\d+): (\\d+)x(\\d+)"
@@ -15,27 +13,25 @@ object Day03 {
         return claims.find { it -> !overlappingIds.contains(it) }!!.id
     }
 
-    private fun parseAndFindOverlappingPoints(input: List<String>): Result {
-        val claims = input.parseWith(PATTERN) { (id, x, y, xSize, ySize) ->
-            Claim(id.toInt(), x.toInt(), y.toInt(), xSize.toInt(), ySize.toInt())
-        }
-        return claims to getPointCounts(claims).filter { it.value.size >= 2 }
-    }
+    private fun parseAndFindOverlappingPoints(input: List<String>) =
+            input.parseWith(PATTERN) { (id, x, y, xSize, ySize) ->
+                Claim(id.toInt(), x.toInt(), y.toInt(), xSize.toInt(), ySize.toInt())
+            }.let { it to getPointCounts(it).filter { points -> points.value.size >= 2 } }
 
-    private fun getPointCounts(input: List<Claim>): Map<Point, Set<Claim>> =
+    private fun getPointCounts(input: List<Claim>) =
             input.map { claim -> claim.getPoints().associateTo(mutableMapOf()) { it to setOf(claim) } }
                     .reduce { a, b -> a.collate(b) { x, y -> x.plus(y) } }
-}
 
-private fun <K, V> MutableMap<K, V>.collate(other: Map<K, V>, reducer: (V, V) -> V): MutableMap<K, V> =
-        this.apply { other.forEach { merge(it.key, it.value, reducer) } }
+    private fun <K, V> MutableMap<K, V>.collate(other: Map<K, V>, reducer: (V, V) -> V) =
+            apply { other.forEach { merge(it.key, it.value, reducer) } }
+}
 
 private data class Claim(val id: Int, val x: Int, val y: Int, val xSize: Int, val ySize: Int) {
     private fun getXRange() = x..(x + xSize - 1)
 
     private fun getYRange() = y..(y + ySize - 1)
 
-    fun getPoints(): Set<Point> = getXRange()
+    fun getPoints() = getXRange()
             .flatMap { x -> getYRange().map { y -> Point(x, y) } }
             .toSet()
 }
--- src/main/kotlin/com/ikueb/advent18/Day04.kt
+++ src/main/kotlin/com/ikueb/advent18/Day04.kt
@@ -8,30 +8,26 @@ object Day04 {
 
     private val PARSER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
 
-    fun strategyOne(input: List<String>): Int {
-        return mapGuardsToNaps(input)
-                .maxBy { it.value.sumBy { nap -> nap.last - nap.first + 1 } }!!
-                .let { it.key * mostCommon(it.value).key }
-    }
+    fun strategyOne(input: List<String>) = mapGuardsToNaps(input)
+            .maxBy { it.value.sumBy { nap -> nap.last - nap.first + 1 } }!!
+            .let { it.key * mostCommon(it.value).key }
 
-    fun strategyTwo(input: List<String>): Int {
-        return mapGuardsToNaps(input)
-                .mapValues { mostCommon(it.value) }
-                .maxBy { it.value.value }!!
-                .let { it.key * it.value.key }
-    }
+    fun strategyTwo(input: List<String>) = mapGuardsToNaps(input)
+            .mapValues { mostCommon(it.value) }
+            .maxBy { it.value.value }!!
+            .let { it.key * it.value.key }
 
     private fun mapGuardsToNaps(input: List<String>): Map<Int, Set<IntRange>> {
         val results = mutableMapOf<Int, Set<IntRange>>()
         var guard: Int by notNull()
-        var asleepAt: Int by notNull()
+        lateinit var asleepAt: Log
         input.parseWith("\\[(.*)\\] (.*)") { (timestamp, entry) ->
             Log(LocalDateTime.parse(timestamp, PARSER), entry)
         }.sorted().forEach { log ->
             when {
                 log.guardEntry != null -> guard = log.guardEntry
-                log.isAsleepEntry -> asleepAt = log.minute
-                log.isAwakeEntry -> results.mergeSetValues(guard, asleepAt..(log.minute - 1))
+                log.isAsleepEntry -> asleepAt = log
+                log.isAwakeEntry -> results.mergeSetValues(guard, asleepAt..log)
             }
         }
         return results
@@ -39,10 +35,9 @@ object Day04 {
 
     private fun mostCommon(set: Set<IntRange>) =
             set.flatten().groupingBy { it }.eachCount().maxBy { it.value }!!
-
 }
 
-data class Log(val timestamp: LocalDateTime, val entry: String) : Comparable<Log> {
+private data class Log(val timestamp: LocalDateTime, val entry: String) : Comparable<Log> {
     val guardEntry = "Guard #(\\d+) begins shift".toRegex()
             .matchEntire(entry)?.let { it.groupValues[1].toInt() }
     val isAwakeEntry = entry == "wakes up"
@@ -50,4 +45,6 @@ data class Log(val timestamp: LocalDateTime, val entry: String) : Comparable<Log
     val minute = timestamp.minute
 
     override fun compareTo(other: Log) = timestamp.compareTo(other.timestamp)
+
+    operator fun rangeTo(other: Log): IntRange = minute..(other.minute - 1)
 }
