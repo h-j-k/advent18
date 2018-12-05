673ad320b45bb5fba678337af263b5b917476235
h-j-k <han.junkwang@gmail.com>
2018-12-06 00:24:33 +0800

    Day 5 plus coroutines support

--- src/main/kotlin/com/ikueb/advent18/Day05.kt
+++ src/main/kotlin/com/ikueb/advent18/Day05.kt
@@ -1,8 +1,16 @@
 package com.ikueb.advent18
 
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
+
 object Day05 {
 
     fun getResultingLength(input: String) = process(input).trim().length
+
+    fun getShortestPossible(input: String) = CharRange('a', 'z')
+            .map { input.replace(it.toString(), "", true) }
+            .parallelMap { process(it) }
+            .minBy { it.length }!!.length
 
     private fun process(input: String): String {
         if (input.length < 2) {
@@ -39,4 +47,8 @@ object Day05 {
     }
 
     private fun Char.isOppositeAscii(other: Char) = Math.abs(toInt() - other.toInt()) == 32
+
+    private fun <T, R> Iterable<T>.parallelMap(mapper: suspend (T) -> R): List<R> = runBlocking {
+        map { async { mapper(it) } }.map { it.await() }
+    }
 }
--- src/test/kotlin/com/ikueb/advent18/Day05Test.kt
+++ src/test/kotlin/com/ikueb/advent18/Day05Test.kt
@@ -1,19 +1,31 @@
 package com.ikueb.advent18
 
 import com.ikueb.advent18.Day05.getResultingLength
+import com.ikueb.advent18.Day05.getShortestPossible
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 internal class Day05Test {
 
     @Test
-    fun canGetResultingLength() {
+    fun partOneExample() {
         assertThat(getResultingLength("dabAcCaCBAcCcaDA")).isEqualTo(10)
     }
 
     @Test
     fun partOne() {
-        val input = getInput("Day05a").joinToString("")
+        val input = getInput("Day05").joinToString("")
         assertThat(getResultingLength(input)).isEqualTo(10804)
+    }
+
+    @Test
+    fun partTwoExample() {
+        assertThat(getShortestPossible("dabAcCaCBAcCcaDA")).isEqualTo(4)
+    }
+
+    @Test
+    fun partTwo() {
+        val input = getInput("Day05").joinToString("")
+        assertThat(getShortestPossible(input)).isEqualTo(6650)
     }
 }
