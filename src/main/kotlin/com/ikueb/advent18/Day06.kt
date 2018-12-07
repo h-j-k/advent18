6d13fc915503e6cf03884f517c7d2e0de826ba6e
h-j-k <han.junkwang@gmail.com>
2018-12-07 22:26:58 +0800

    More refactoring on Day 6

--- src/main/kotlin/com/ikueb/advent18/Day06.kt
+++ src/main/kotlin/com/ikueb/advent18/Day06.kt
@@ -7,7 +7,7 @@ object Day06 {
     fun getLargestDefiniteArea(input: List<String>): Int {
         val points = input.parseWith("(\\d+), (\\d+)") { (x, y) -> Point(x, y) }
         val boundary = getBoundary(points)
-        return boundary.getEnclosing()
+        return boundary.getEnclosingPoints()
                 .associateWith { point ->
                     points.associateWith(point::manhattanDistance)
                             .flip()
@@ -16,43 +16,42 @@ object Day06 {
                 }
                 .filterValues { it != null }
                 .flip()
-                .filterValues { closest -> closest.none { it.on(boundary) } }
+                .filterValues { closest -> closest.none(boundary::contains) }
                 .map { it.value.size }
                 .maxBy { it }!!
     }
 
     fun getLargestRegionWithManhattanDistanceSumLessThan(input: List<String>, sum: Int): Int {
         val points = input.parseWith("(\\d+), (\\d+)") { (x, y) -> Point(x, y) }
-        return getBoundary(points).getEnclosing()
+        return getBoundary(points).getEnclosingPoints()
                 .associateWith { point -> points.sumBy(point::manhattanDistance) }
                 .filterValues { it < sum }
                 .size
     }
 
     private fun getBoundary(points: Collection<Point>): Boundary {
-        var x1: Int by vetoable(Int.MAX_VALUE) { _, old, new -> new < old }
-        var x2: Int by vetoable(Int.MIN_VALUE) { _, old, new -> new > old }
-        var y1: Int by vetoable(Int.MAX_VALUE) { _, old, new -> new < old }
-        var y2: Int by vetoable(Int.MIN_VALUE) { _, old, new -> new > old }
+        var xMin: Int by vetoable(Int.MAX_VALUE) { _, old, new -> new < old }
+        var xMax: Int by vetoable(Int.MIN_VALUE) { _, old, new -> new > old }
+        var yMin: Int by vetoable(Int.MAX_VALUE) { _, old, new -> new < old }
+        var yMax: Int by vetoable(Int.MIN_VALUE) { _, old, new -> new > old }
         points.forEach {
-            x1 = it.x
-            x2 = it.x
-            y1 = it.y
-            y2 = it.y
+            xMin = it.x
+            xMax = it.x
+            yMin = it.y
+            yMax = it.y
         }
-        return Point(x1, y1) to Point(x2, y2)
+        return Point(xMin, yMin) to Point(xMax, yMax)
     }
 }
 
 private typealias Boundary = Pair<Point, Point>
 
-private fun Boundary.getEnclosing() =
+private fun Boundary.getEnclosingPoints() =
         (first.x..second.x)
                 .flatMap { x -> (first.y..second.y).map { y -> Point(x, y) } }
                 .toSet()
 
-private fun Point.on(boundary: Boundary) = boundary.let { (min, max) ->
-    x == min.x || x == max.x || y == min.y || y == max.y
-}
+private fun Boundary.contains(point: Point) =
+        point.x == first.x || point.x == second.x || point.y == first.y || point.y == second.y
 
 private fun Point.manhattanDistance(other: Point) = Math.abs(x - other.x) + Math.abs(y - other.y)
