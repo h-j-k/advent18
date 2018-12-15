96c5724ddaf2f1721d25d9612eda841997b1e0b3
h-j-k <han.junkwang@gmail.com>
2018-12-15 17:52:48 +0800

    Day 11 refactoring

--- src/main/kotlin/com/ikueb/advent18/Day11.kt
+++ src/main/kotlin/com/ikueb/advent18/Day11.kt
@@ -1,33 +1,32 @@
 package com.ikueb.advent18
 
-import com.ikueb.advent18.Day11.range
-
 object Day11 {
 
-    val range = (1..300)
-    private val subRange = (2..299)
+    private val range = (1..300)
+    private val boundary = Boundary(Point(1, 1), Point(300, 300))
 
     fun getTopSquareTopCorner(input: Int) =
-            with(generateGrid(input)) {
-                filterKeys { it.x in subRange && it.y in subRange }
-                        .mapValues { (_, cell) -> cell.withNearbyPowerSum(this) }
-                        .maxBy { entry -> entry.value }!!
-                        .let { (point, _) -> Point(point.x - 1, point.y - 1) }
-            }
+            process(input) { point ->
+                if (boundary.isOnBoundary(point)) emptySequence() else sequenceOf(3)
+            }.first
 
     fun getTopVariableSquareTopCornerSize(input: Int) =
+            process(input).let { (corner, size, _) ->
+                String.format("%d,%d,%d", corner.x, corner.y, size)
+            }
+
+    private fun process(input: Int,
+                        toSequence: (Point) -> Sequence<Int> = Point::squareSizesTo) =
             generateGrid(input).entries
                     .fold(mutableMapOf<Point, Int>()) { grid, entry ->
-                        grid[entry.key] = entry.value.withCumulativeSum(grid); grid
-                    }.run {
-                        keys.asSequence().flatMap {
-                            it.squareSizesTo().map { size ->
-                                Triple(it.nw(size - 1), size, squarePowerTo(this, it, size))
+                        grid.also { it[entry.key] = entry.value.cumulativeSum(it) }
+                    }.let {
+                        it.keys.asSequence().flatMap { point ->
+                            toSequence(point).map { size ->
+                                Triple(point.nw(size - 1), size,
+                                        squarePowerTo(it, point, size))
                             }
                         }.maxBy { (_, _, power) -> power }!!
-                                .let { (corner, size, _) ->
-                                    String.format("%d,%d,%d", corner.x, corner.y, size)
-                                }
                     }
 
     private fun generateGrid(serial: Int) = range
@@ -49,21 +48,14 @@ private data class Cell(val point: Point, val value: Int) {
                 ((((it * y) + serial) * it / 100) % 10) - 5
             })
 
-    fun withNearbyPowerSum(grid: Map<Point, Cell>) =
-            getNearbyPoints().sumBy { grid[it]?.value ?: 0 }
-
-    fun withCumulativeSum(grid: Map<Point, Int>) =
+    fun cumulativeSum(grid: Map<Point, Int>) =
             value + setOf(point.n(), point.w()).sumBy {
                 grid[it] ?: 0
             } - (grid[point.nw()] ?: 0)
-
-    private fun getNearbyPoints() =
-            (-1..1).flatMap { x ->
-                (-1..1).map { y ->
-                    Point((point.x + x).coerceIn(range),
-                            (point.y + y).coerceIn(range))
-                }
-            }
 }
 
 private fun Point.squareSizesTo() = (1..minOf(x, y)).asSequence()
+
+private fun Boundary.isOnBoundary(point: Point) = with(point) {
+    x == first.x || x == second.y || y == first.y || y == second.y
+}
