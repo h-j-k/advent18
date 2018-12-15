c9f56be19fa0f642f9ae0ab618b2b773d821f0ae
h-j-k <han.junkwang@gmail.com>
2018-12-16 00:47:49 +0800

    Day 12 part 2

--- src/main/kotlin/com/ikueb/advent18/Day12.kt
+++ src/main/kotlin/com/ikueb/advent18/Day12.kt
@@ -2,28 +2,45 @@ package com.ikueb.advent18
 
 object Day12 {
 
-    fun getSumOfPlantedPots(input: List<String>): Int {
+    fun getSumOfPlantedPots(input: List<String>) =
+            process(input, (1..20).asSequence(), 0).toInt()
+
+    fun getSumOfPlantedPotsAt(input: List<String>, target: Long) =
+            process(input = input, target = target)
+
+    private fun process(input: List<String>,
+                        sequence: Sequence<Int> = generateSequence(1) { it + 1 },
+                        target: Long): Long {
         val state = input[0].replace("initial state: ", "")
                 .mapIndexed { i, c -> i to (c == '#') }.toMap(mutableMapOf())
         val rules = input.drop(2).map { PlantPredicate(it) }
-        for (i in 1..20) {
+        var previous = setOf<Int>()
+        var last = 0
+        for (i in sequence) {
             val stateCopy = state.toMap()
             val start = stateCopy.keys.minBy { it }!! - 3
             val end = stateCopy.keys.maxBy { it }!! + 3
-            (start..end).associateWith { n ->
+            val current = (start..end).associateWith { n ->
                 rules.map { it.isPlantNext(n, stateCopy) }.firstOrNull()
-            }.forEach { state[it.key] = it.value ?: false }
+            }.mapValues { it.value ?: false }
+                    .apply { state.putAll(this) }
+                    .filterValues { it }.keys
+            if (current.map { it - 1 }.toSet() == previous) {
+                last = i
+                break
+            }
+            previous = current
         }
-        return state.filterValues { it }.keys.sum()
+        return state.filterValues { it }.keys
+                .map { it + (target - last) }
+                .reduce(Long::plus)
     }
 }
 
-data class PlantPredicate(val plants: List<Boolean>,
-                          val outcome: Boolean) {
+data class PlantPredicate(val plants: List<Boolean>, val outcome: Boolean) {
 
-    constructor(input: String) : this(
-            input.substring(0, 5).map { it == '#' },
-            input.endsWith("#"))
+    constructor(input: String) :
+            this(input.substring(0, 5).map { it == '#' }, input.endsWith("#"))
 
     fun isPlantNext(n: Int, stateCopy: Map<Int, Boolean>) =
             if ((-2..2).map { n + it }.all {
--- src/test/kotlin/com/ikueb/advent18/Day12Test.kt
+++ src/test/kotlin/com/ikueb/advent18/Day12Test.kt
@@ -1,6 +1,7 @@
 package com.ikueb.advent18
 
 import com.ikueb.advent18.Day12.getSumOfPlantedPots
+import com.ikueb.advent18.Day12.getSumOfPlantedPotsAt
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -14,6 +15,12 @@ internal class Day12Test {
     @Test
     fun partOne() {
         assertThat(getSumOfPlantedPots(getInput("Day12"))).isEqualTo(6201)
+    }
+
+    @Test
+    fun partTwo() {
+        assertThat(getSumOfPlantedPotsAt(getInput("Day12"), 50000000000))
+                .isEqualTo(9300000001023)
     }
 
     private val example = listOf(
