56c1244cbaef90cea414b38d8e7866f4f696751b
h-j-k <han.junkwang@gmail.com>
2018-12-23 14:14:00 +0800

    Day 18 part 2 refactoring

--- src/main/kotlin/com/ikueb/advent18/Day18.kt
+++ src/main/kotlin/com/ikueb/advent18/Day18.kt
@@ -50,13 +50,13 @@ private data class Acre(
 
     override fun toString() = content.output.toString()
 
-    fun next(state: Area) = when (val now = content) {
+    fun next(state: Area) = when (content) {
         Content.OPEN ->
             if (state.near(this, 3, Content.TREES)) Content.TREES
-            else now
+            else content
         Content.TREES ->
             if (state.near(this, 3, Content.LUMBERYARD)) Content.LUMBERYARD
-            else now
+            else content
         Content.LUMBERYARD ->
             if (state.near(this, 1, Content.LUMBERYARD)
                     && state.near(this, 1, Content.TREES)) Content.LUMBERYARD
