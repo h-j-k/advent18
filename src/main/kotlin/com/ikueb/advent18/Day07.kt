fc141d14ea8231cf2595ee8fdad8f6d55cd8ec0b
h-j-k <han.junkwang@gmail.com>
2018-12-24 17:40:23 +0800

    Day 7 part 2

--- src/main/kotlin/com/ikueb/advent18/Day07.kt
+++ src/main/kotlin/com/ikueb/advent18/Day07.kt
@@ -4,32 +4,31 @@ object Day07 {
 
     private const val DEFINITION = "Step (.) must be finished before step (.) can begin."
 
-    fun getOrderedSteps(input: List<String>): String {
+    fun getOrderedSteps(input: List<String>,
+                        total: Int = 1,
+                        effort: (String) -> Int = { 1 }): Pair<String, ElfWorkers> {
         val steps = input.parseWith(DEFINITION) { (dependsOn, step) ->
             Step(step = step, dependsOn = dependsOn)
         }
-        val dependents = mutableMapOf<String, MutableList<String>>()
-        val dependencies = mutableMapOf<String, MutableList<String>>()
+        val dependents: Tracker = mutableMapOf()
+        val dependencies: Tracker = mutableMapOf()
         steps.forEach {
             dependents.mergeMutableListValues(it.dependsOn, it.step)
             dependencies.mergeMutableListValues(it.step, it.dependsOn)
         }
-        val result = mutableListOf<String>()
-        val start = dependents
-                .filter { (key, _) -> !dependents.flatMap { it.value }.contains(key) }
-        val toProcess = start.keys.sorted().toMutableSet()
-        while (toProcess.isNotEmpty()) {
-            val n = toProcess.sortAndPop()
-            result.add(n)
-            (dependents[n] ?: mutableListOf()).forEach { dependent ->
-                dependencies.computeIfPresent(dependent) { _, value ->
-                    value.remove(n)
-                    if (value.isEmpty()) null else value
-                }.let { if (it == null) toProcess.add(dependent) }
+        val target = steps.flatMap { listOf(it.step, it.dependsOn) }.distinct().count()
+        val toProcess = dependents
+                .filter { (key, _) -> !dependents.flatMap { it.value }.contains(key) }.keys
+                .sorted()
+                .toMutableSet()
+        val workers = ElfWorkers(total, effort)
+        with(mutableListOf<String>()) {
+            while (size < target) {
+                workers.inWith(toProcess)
+                addAll(workers.outAs(dependents, dependencies, toProcess))
             }
-
+            return joinToString("") to workers
         }
-        return result.joinToString("")
     }
 }
 
@@ -37,3 +36,47 @@ private fun <T : Comparable<T>> MutableSet<T>.sortAndPop() =
         sortedBy { it }[0].also { remove(it) }
 
 private data class Step(val step: String, val dependsOn: String)
+
+private typealias Tracker = MutableMap<String, MutableList<String>>
+
+data class ElfWorkers(val total: Int, val effort: (String) -> Int) {
+
+    private val workload = mutableMapOf<String, Int>()
+    private var counter = 0
+
+    fun getTimeTaken() = counter
+
+    fun inWith(toProcess: MutableSet<String>) {
+        while (total - workload.size > 0 && toProcess.isNotEmpty()) {
+            val n = toProcess.sortAndPop()
+            workload[n] = effort(n)
+        }
+    }
+
+    fun outAs(dependents: Tracker,
+              dependencies: Tracker,
+              toProcess: MutableSet<String>): List<String> {
+        val done = mutableListOf<String>()
+        workload.keys.toSet().forEach { key ->
+            workload.computeAndProcess(key, { _, value ->
+                if (value == 1) null else value - 1
+            }, {
+                (dependents[key] ?: mutableListOf()).forEach { dependent ->
+                    dependencies.computeAndProcess(dependent, { _, value ->
+                        value.remove(key)
+                        if (value.isEmpty()) null else value
+                    }, { toProcess.add(dependent) })
+                }
+                done.add(key)
+            })
+        }
+        counter++
+        return done.toList()
+    }
+}
+
+private fun <K, V> MutableMap<K, V>.computeAndProcess(key: K,
+                                                      remapping: (K, V) -> V?,
+                                                      processor: () -> Unit) {
+    computeIfPresent(key, remapping).let { if (it == null) processor() }
+}
--- src/test/kotlin/com/ikueb/advent18/Day07Test.kt
+++ src/test/kotlin/com/ikueb/advent18/Day07Test.kt
@@ -6,15 +6,35 @@ import org.junit.jupiter.api.Test
 
 internal class Day07Test {
 
+    private val partOne: (Pair<String, ElfWorkers>) -> String = { it.first }
+    private val partTwo: (Pair<String, ElfWorkers>) -> Int = { it.second.getTimeTaken() }
+
     @Test
     fun partOneExample() {
-        assertThat(getOrderedSteps(example)).isEqualTo("CABDFE")
+        assertThat(getOrderedSteps(example).let(partOne))
+                .isEqualTo("CABDFE")
     }
 
     @Test
     fun partOne() {
-        assertThat(getOrderedSteps(getInput("Day07"))).isEqualTo("OKBNLPHCSVWAIRDGUZEFMXYTJQ")
+        assertThat(getOrderedSteps(getInput("Day07")).let(partOne))
+                .isEqualTo("OKBNLPHCSVWAIRDGUZEFMXYTJQ")
     }
+
+    @Test
+    fun partTwoExample() {
+        assertThat(getOrderedSteps(example, 2) { it.toValue() }.let(partTwo))
+                .isEqualTo(15)
+    }
+
+    @Test
+    fun partTwo() {
+        assertThat(getOrderedSteps(getInput("Day07"), 4) { it.toValue(60) }.let(partTwo))
+                .isEqualTo(982)
+    }
+
+    private fun String.toValue(base: Int = 0) =
+            base + 1 + this[0].toInt() - 'A'.toInt()
 
     private val example = listOf(
             "Step C must be finished before step A can begin.",
