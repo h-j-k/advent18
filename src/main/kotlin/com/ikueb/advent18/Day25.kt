c2550d8d7167fe8c6a8120423771c9191856aba3
h-j-k <han.junkwang@gmail.com>
2018-12-31 00:00:14 +0800

    Day 25 refactoring

--- src/main/kotlin/com/ikueb/advent18/Day25.kt
+++ src/main/kotlin/com/ikueb/advent18/Day25.kt
@@ -7,15 +7,17 @@ object Day25 {
     private const val DEFINITION = "([-\\d ]+),([-\\d ]+),([-\\d ]+),([-\\d ]+)"
 
     fun getConstellations(input: List<String>) = with(
-            input.parseWith(DEFINITION) { (a, b, c, d) -> Point4d(a, b, c, d) }) {
+            input.parseWith(DEFINITION) { (a, b, c, d) ->
+                Point4d(a.trimInt(), b.trimInt(), c.trimInt(), d.trimInt())
+            }) {
         associateWith { point -> filter { point.isNearby(it) }.toSet() }
     }.let { formConstellations(it).count() }
 
     private fun formConstellations(nearby: Map<Point4d, Set<Point4d>>) = sequence {
-        val remaining = nearby.keys.toCollection(ArrayDeque<Point4d>())
+        val remaining = nearby.keys.toDeque()
         while (remaining.isNotEmpty()) {
             val (current, toProcess) = with(remaining.removeFirst()) {
-                mutableSetOf(this) to ArrayDeque<Point4d>(nearby[this]!!)
+                mutableSetOf(this) to nearby[this]!!.toDeque()
             }
             while (toProcess.isNotEmpty()) {
                 toProcess.removeFirst().let { point ->
@@ -31,12 +33,13 @@ object Day25 {
 
 private data class Point4d(val a: Int, val b: Int, val c: Int, val d: Int) {
 
-    constructor(a: String, b: String, c: String, d: String) :
-            this(a.trim().toInt(), b.trim().toInt(), c.trim().toInt(), d.trim().toInt())
-
     fun isNearby(other: Point4d) = this != other && manhattanDistance(other) <= 3
 
     private fun manhattanDistance(other: Point4d) =
             listOf(a - other.a, b - other.b, c - other.c, d - other.d)
                     .sumBy { Math.abs(it) }
 }
+
+private fun String.trimInt() = trim().toInt()
+
+private fun <T> Iterable<T>.toDeque() = toCollection(ArrayDeque<T>())
